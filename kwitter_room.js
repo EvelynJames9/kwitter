831fa8140aab3003b1a35163c039fc71829d71e8
EvelynJames9 <98927398+EvelynJames9@users.noreply.github.com>
2022-06-04 20:58:54 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -31,8 +31,8 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
     Room_names = childKey;
    //Start code
    console.log("Room Name - " + Room_names);
-   row = "<div class='room_name'id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names +"</div><hr>";
-   document.getElementById(output).innerHTML += row;
+   row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+     document.getElementById(output).innerHTML += row;
    //End code
    });});}
 getData();
@@ -43,10 +43,10 @@ function redirectToRoomName(name){
    window.location = "kwitter_page.html";
 }
 
-function logOut(){
+function logout(){
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
-      window.location = "kwitter.html";
+      window.location.replace = "index.html";
 }
 
 
